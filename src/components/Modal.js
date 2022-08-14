d26758fd4c75f8b3bb2d8dd712358cd3acf72601
mahmoud fouad <mfapolice@gmail.com>
2022-08-15 00:02:22 +0200

    practice

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,13 @@
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
       <p>are you sure</p>
-      <button className="btn btn--alt">cancel</button>
-      <button className="btn">confirm</button>
+      <button className="btn btn--alt" onClick={props.onChange}>
+        cancel
+      </button>
+      <button className="btn" onClick={props.onChange}>
+        confirm
+      </button>
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,18 +3,31 @@ import Modal from './Modal';
 import Backdrop from './Backdrop';
 
 function Todo(props) {
-  const [] = useState(false);
-  function deleteHandler(e) {
-    e.currentTarget.remove();
+  // const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  function deleteBtnHandler(e) {
+    // e.currentTarget.remove();
+    // setModalIsOpen(true);
+    setModalIsOpen(true);
   }
+
+  ///////////////////////////
+  // function closeModal() {
+  //   setModalIsOpen(false);
+  // }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>
+        <button className="btn" onClick={deleteBtnHandler}>
           {props.btn}
         </button>
       </div>
+      {/* {modalIsOpen && <Modal onChange={closeModal} />}
+      {modalIsOpen && <Backdrop onChange={closeModal} />} */}
+      {<Modal />}
+      {<Backdrop />}
     </div>
   );
 }
