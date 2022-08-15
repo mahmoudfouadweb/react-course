ed8990085c4fc1cf715b7e6499c6c4e5d2867bc9
mahmoud fouad <mfapolice@gmail.com>
2022-08-15 03:17:22 +0200

    another practical project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import Todo from './components/Todo';
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="react" btn="delete" />
-      <Todo text="css" btn="open" />
-      <Todo text="html" btn="close" />
-      <Todo text="css" btn="open" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/components/Backdrop.js
+++
@@ -1,5 +0,0 @@
-function Backdrop(props) {
-  return <div className="backdrop" onClick={props.onChange}></div>;
-}
-
-export default Backdrop;
